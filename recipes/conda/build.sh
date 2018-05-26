2b4fcd4429a5df4579b8fbea63f017e56b6f44e4
Omar Laurino <olaurino@cfa.harvard.edu>
2018-05-26 13:10:43 -0400

    add windows batch file

--- recipes/conda/build.sh
+++ recipes/conda/build.sh
@@ -4,6 +4,7 @@ mvn package -DskipTests
 export JOVIAL_DIR=${PREFIX}/opt/jovial
 export JOVIAL_JAR=${JOVIAL_DIR}/jovial.jar
 export JOVIAL_BIN=${PREFIX}/bin/jovial
+export JOVIAL_WIN_BIN=${PREFIX}/bin/jovial.bat
 
 mkdir -p ${JOVIAL_DIR}
 cp target/jovial*dependencies*.jar ${JOVIAL_JAR}
@@ -12,6 +13,12 @@ cat << EOF >> ${JOVIAL_BIN}
 #!/bin/bash
 
 java -jar ${JOVIAL_JAR} "\$@"
+
+EOF
+
+cat << EOF >> ${JOVIAL_WIN_BIN}
+
+java -jar ${JOVIAL_JAR} %*
 
 EOF
 
